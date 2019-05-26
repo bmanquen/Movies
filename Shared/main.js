6a989a2f162c27560a841adee1f9043d24090cd1
Brenden Manquen <bmanquen1993@ou.edu>
2019-05-26 10:10:51 -0700

    Initial Commit

---
+++ Shared/main.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    var url = window.location.href.split('/');
+    var loc = url[url.length - 1];
+    var link = document.querySelectorAll('nav a');
+
+    for (var i = 0; i < link.length; i++) {
+        var path = link[i].href.split('/');
+        var page = path[path.length - 1];
+        if (page == loc || page == 'home.cshtml' && loc == '') {
+            link[i].parentNode.className += ' ' + 'active';
+            document.body.className += ' ' + page.substr(0, page.lastIndexOf('.'));
+        }
+    }
+});
